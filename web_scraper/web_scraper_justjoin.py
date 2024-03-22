c77dca1261e8bc3faee6b327a84a81fde8c067d1
Jakub <banaszek.jakub1@gmail.com>
2024-03-22 15:06:07 +0100

    version 1.0

--- web_scraper/web_scraper_justjoin.py
+++ web_scraper/web_scraper_justjoin.py
@@ -1,42 +1,43 @@
 from bs4 import BeautifulSoup
 import requests
 
-job_offer_quantity = 1
-ulr_page = "https://justjoin.it/"
 
-
-def get_offer(url, offer_quantity):
-    page = requests.get(url)
+def get_offer(url_page, scrapper_offers):
+    page = requests.get(url_page)
     soup = BeautifulSoup(page.content, 'html.parser')
 
-    job_list = soup.find_all('div', class_='css-2crog7')[:offer_quantity]
+    job_list = soup.find_all('div', class_='css-2crog7')[:scrapper_offers]
+
+    job_offers = []  # List to store multiple job offers
 
     for job in job_list:
         job_link = job.find("a", class_="offer_list_offer_link css-4lqp8g")
         href = job_link.get('href')
         if href:
-            full_url = url + href
+            full_url = url_page + href
             single_job_page = requests.get(full_url)
             single_job_soup = BeautifulSoup(single_job_page.content, 'html.parser')
+
+            offer_data = {}  # Dictionary to store data for a single job offer
 
             # Extracting title from job page
             title = single_job_soup.find("div", class_="css-vb54bv")
             if title:
                 h1_tag = title.find('h1')
-                h1_tag_text = h1_tag.get_text(strip=True)
-                return h1_tag_text
+                offer_data['title'] = h1_tag.get_text(strip=True)
 
             # Extracting company from job page
             company = single_job_soup.find('div', class_='css-mbkv7r')
-            span_text_company = company.get_text(strip=True) if company else "No company found"
+            offer_data['company'] = company.get_text(strip=True) if company else "No company found"
 
             # Extracting type of work, experience, employment type, operating mode
             description_work = {}
             type_work = single_job_soup.find_all("div", class_="css-6q28fo")
-            for type in type_work:
-                description = type.find('div', class_="css-qyml61").text
-                rest = type.find('div', class_="css-15wyzmd").text
+            for data in type_work:
+                description = data.find('div', class_="css-qyml61").text
+                rest = data.find('div', class_="css-15wyzmd").text
                 description_work[description] = rest
+            offer_data['description'] = description_work
 
             # Extracting stack
             stack_list = {}
@@ -47,19 +48,21 @@ def get_offer(url, offer_quantity):
                                               class_="MuiTypography-root MuiTypography-subtitle4 css-1wcj8lw").text
 
                 stack_list[technology] = technology_level
+            offer_data['stack'] = stack_list
 
         # Extracting earnings from job page
-        money = job.find('div', class_='css-17pspck')
-        if money:
-            span_text = money.find_all('span')
+        job_earnings = job.find('div', class_='css-17pspck')
+        if job_earnings:
+            span_text = job_earnings.find_all('span')
             if len(span_text) >= 2:
                 first_text = span_text[0].get_text(strip=True)
                 second_text = span_text[1].get_text(strip=True)
-                return first_text, second_text
+                offer_data['earnings'] = f"{first_text} {second_text}"
             else:
-                return False
+                offer_data['earnings'] = 'Undisclosed salary'
         else:
-            return False
+            offer_data['earnings'] = 'Undisclosed salary'
 
+        job_offers.append(offer_data)
 
-get_offer(ulr_page, job_offer_quantity)
+    return job_offers
