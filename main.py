09ea263fb98c05b2e11a46b329ad10734a3010bf
Jakub <banaszek.jakub1@gmail.com>
2024-03-22 15:21:26 +0100

    add main function

---
+++ main.py
@@ -0,0 +1,27 @@
+from web_scraper.web_scraper_justjoin import get_offer
+from web_scraper.data.db_offers import JobOffersDB
+from web_scraper.data.transformation_data import transform_data
+
+# Configuration variables
+scrapper_offers = 1
+url_page = "https://justjoin.it/"
+
+# Database configuration
+name = 'demo'
+user = 'postgres'
+password = '123'
+host = 'localhost'
+port = '5432'
+table = 'job_offers'
+
+
+def main():
+    job_list = get_offer(url_page, scrapper_offers)
+    job_offers_db = JobOffersDB(name, user, password, host, port, table)
+    for job in job_list:
+        clean_data = transform_data(job)
+        job_offers_db.insert_job_offer(clean_data)
+
+
+if __name__ == "__main__":
+    main()
