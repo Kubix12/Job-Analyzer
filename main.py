1e602499671a13c6581531763a046e5881f34fcd
Jakub <banaszek.jakub1@gmail.com>
2024-03-28 09:35:17 +0100

    Change connection with database / create db_configuration.py

--- main.py
+++ main.py
@@ -1,18 +1,11 @@
 from web_scraper.web_scraper_justjoin import get_offer
 from web_scraper.data.db_offers import JobOffersDB
 from web_scraper.data.transformation_data import transform_data
+from web_scraper.data.db_configuration import name, user, password, host, port, table
 
 # Configuration variables
 scrapper_offers = 1
 url_page = "https://justjoin.it/"
-
-# Database configuration
-name = 'postgres'
-user = 'postgres'
-password = '123'
-host = 'localhost'
-port = '5432'
-table = 'job_offers'
 
 
 def main():
